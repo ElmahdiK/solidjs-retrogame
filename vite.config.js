385d368021d95813d420d12d728baf336e9ed3e8
ElmahdiK <elmahdi.korfed@gmail.com>
2024-08-10 16:27:30 +0200

    fix: vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,12 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  base: 'solidjs-retrogame',
+  plugins: [solidPlugin()],
+  base: '/solidjs-retrogame/',
   server: {
     port: 3000,
   },
-  build: {
-    target: 'esnext',
-  },
+  // build: {
+  //   target: 'esnext',
+  // },
 });
